5092b2d9e48b462a3564499f509ee5a9ac63021b
carlos.pardo.duran <carlos.pardo.duran@gmail.com>
2013-05-26 14:27:42 +0200

    v1

--- PruebaSpring/src/test/java/org/assembly/dao/Prueba.java
+++ PruebaSpring/src/test/java/org/assembly/dao/Prueba.java
@@ -1,49 +1,49 @@
-package org.assembly.dao;
-
-import static org.junit.Assert.assertEquals;
-
-import javax.annotation.Resource;
-
-import org.assembly.model.Person;
-import org.junit.runner.RunWith;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
-
-@ContextConfiguration(locations = "classpath:applicationContext.xml")
-@RunWith(SpringJUnit4ClassRunner.class)
-public class Prueba {
-
-	@Resource
-	private static PersonDAO dao;
-	
-	public void setDao(PersonDAO dao) {
-		this.dao = dao;
-	}
-	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		
-		
-		save();
-		
-		
-	}
-	
-	@Transactional
-	public static void save() {
-		
-		Person person = new Person("Pablo", "Goette");
-		
-		dao.save(person);
-		
-		Person personSaved = dao.get(person.getId());
-		
-		assertEquals(person, personSaved);
-	}
-
-}
+//package org.assembly.dao;
+//
+//import static org.junit.Assert.assertEquals;
+//
+//import javax.annotation.Resource;
+//
+//import org.assembly.model.Person;
+//import org.junit.runner.RunWith;
+//import org.springframework.stereotype.Component;
+//import org.springframework.stereotype.Service;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@ContextConfiguration(locations = "classpath:applicationContext.xml")
+//@RunWith(SpringJUnit4ClassRunner.class)
+//public class Prueba {
+//
+//	@Resource
+//	private static PersonDAO dao;
+//	
+//	public void setDao(PersonDAO dao) {
+//		this.dao = dao;
+//	}
+//	
+//	
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//
+//		
+//		
+//		save();
+//		
+//		
+//	}
+//	
+//	@Transactional
+//	public static void save() {
+//		
+//		Person person = new Person("Pablo", "Goette");
+//		
+//		dao.save(person);
+//		
+//		Person personSaved = dao.get(person.getId());
+//		
+//		assertEquals(person, personSaved);
+//	}
+//
+//}
